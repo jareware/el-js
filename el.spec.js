b556ca68eb51b9e4b28b0f8279863ed609de34e7
Jarno Rantanen <jarno@jrw.fi>
2015-11-11 00:11:06 +0200

    Add simplest possible test case.

--- el.spec.js
+++ el.spec.js
@@ -1,7 +1,26 @@
+var el = require('./el');
+var outputEl = document.querySelector('#el-js');
+
+function assertDomContent(elTree, expected) {
+    el(outputEl, elTree);
+    var actual = outputEl.innerHTML.replace(/\s+/g, ' ').trim();
+    if (actual === expected) return;
+    throw new Error('Unexpected element output; to debug this, break on errors and inspect the DOM');
+}
+
+function resetOutputEl() {
+    outputEl.innerHTML = '';
+}
+
 describe('el.js', function() {
 
-    it('works', function() {
-        // TODO
+    afterEach(resetOutputEl);
+
+    it('produces the simplest possible element', function() {
+        assertDomContent(
+            el('div', 'Hello World'),
+            '<div>Hello World</div>'
+        );
     });
 
 });
